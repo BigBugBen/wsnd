1439758be4e78fa717c8e39eaa1cfef549485a65
bigben <wb-lzl843922@alibaba-inc.com>
2020-11-25 10:30:17 +0800

    12345

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// http.HandleFunc("/", HelloServer)
+	// http.ListenAndServe(":3030", nil)
+	fmt.Println("ctmd " + `\`)
+}
+
+// func HelloServer(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, zzy's father!")
+// }
